39f427051d4d4e689934105324d24899865723b9
Ye Huang <ye.huang@itc-engineering.com>
2014-02-25 10:46:26 +0100

    update js scenario

--- scripts/irisgm_cindex.js
+++ scripts/irisgm_cindex.js
@@ -1,5 +1,7 @@
+print("start: " + new Date());
 for (var i = 1; i <= 10000000; i++) {
   var numbervalue = i % 1000;
   var datevalue = "2014-02-25 12:00:00";
   db.gm_std_measurements_coveringindex.insert({ "fkDataSeriesId" : numbervalue, "measDateUtc" : datevalue, "measDateSite" : datevalue, "project_id" : numbervalue, "measvalue" : numbervalue, "refMeas" : false, "reliability" : 1 });
 }
+print("end: " + new Date());
