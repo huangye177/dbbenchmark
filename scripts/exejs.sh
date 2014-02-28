3d1afc689ec2f920bfa895354dd89e19fcc2f763
Ye Huang <ye.huang@itc-engineering.com>
2014-02-28 14:43:42 +0100

    update js for mess import

---
+++ scripts/exejs.sh
@@ -0,0 +1,1 @@
+mongo localhost:27017/testdb target.js
--- scripts/irisgm_1b_no_cindex.js
+++ scripts/irisgm_1b_no_cindex.js
@@ -1,5 +1,5 @@
 print("start: " + new Date());
-for (var i = 1; i <= 10000000; i++) {
+for (var i = 1; i <= 1000000000; i++) {
   var numbervalue = i % 1000;
   var datevalue = "2014-02-25 12:00:00";
   db.gm_std_measurements.insert({ "fkDataSeriesId" : numbervalue, "measDateUtc" : datevalue, "measDateSite" : datevalue, "project_id" : numbervalue, "measvalue" : numbervalue, "refMeas" : false, "reliability" : 1 });
